9ce167df667492de0467f33cdc98e1e09f2f38b5
David <david.ngu3@gmail.com>
2020-11-23 19:18:47 +1100

    set up html and js scripts

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 // gameboard module 
 var gameBoard = (function() {
-    var board = []; // tic tac toe board
+    var board = [['X', 'O', 'O'], ['O', 'X', 'X'], ['X', 'O', 'O']]; // tic tac toe board
     
     return {
         board
-    }
+    };
 })();
 
+
+// displayController module
+var displayController = (function() {
+    var renderBoard = function() {
+        var container = document.getElementById('board-container');
+        var board = gameBoard.board;
+        
+    };
+
+    return { 
+        renderBoard
+    };
+})();
+
+// player factory
+var player = function() {
+    var score = 0;
+
+    return {
+        score
+    };
+}
+
+var tempbtn = document.getElementById('temprender');
+
+tempbtn.addEventListener('click', () => {
+    displayController.renderBoard();
+});
